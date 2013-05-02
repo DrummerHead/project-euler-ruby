1459c14458fea147a79e1246a6d0165df1305e2c
DrummerHead <drummerhead@gmail.com>
2013-05-01 21:27:19 -0300

    Refactor, no need to check all numbers

--- 5.rb
+++ 5.rb
@@ -7,24 +7,25 @@
 
 
 
-max_dividends = 77777777777777
+max_dividends_seed = 77777777777777
 max_divisors = 20
 
 
 evenly_divisible = catch :gotit do
 
-  for i in 1..max_dividends do
+  for i in 1..max_dividends_seed do
+    dividend = i * max_divisors
     divisible_count = 0
 
     for j in 2..max_divisors do
-      if i%j == 0
+      if dividend % j == 0
         divisible_count = divisible_count + 1
       else
         break
       end
 
       if divisible_count == max_divisors - 1
-        throw :gotit, i
+        throw :gotit, dividend
       end
     end
 
@@ -35,3 +36,4 @@ end
 puts "tha number is #{evenly_divisible}"
 
 # WHOOOOAAA!! This solution is FUCKING SLOW!!
+# Edit: Some refactor made it slightly less sucking... still slow
