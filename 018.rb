fb5f64a685bffd6f292b9aee2d881bd6e1c2abd9
DrummerHead <drummerhead@gmail.com>
2013-08-09 20:21:56 -0300

    Raw pyramid solution

--- 018.rb
+++ 018.rb
@@ -1,17 +1,125 @@
-tri = [
-  [75],
-  [95, 64],
-  [17, 47, 82],
-  [18, 35, 87, 10],
-  [20,  4, 82, 47, 65],
-  [19,  1, 23, 75,  3, 34],
-  [88,  2, 77, 73,  7, 63, 67],
-  [99, 65,  4, 28,  6, 16, 70, 92],
-  [41, 41, 26, 56, 83, 40, 80, 70, 33],
-  [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
-  [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
-  [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
-  [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
-  [63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
-  [04, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]
-]
+require './018_data.rb'
+require './067_data.rb'
+
+
+@arroyo_seco = []
+
+def recumagia(pyramid, x, y, val)
+  if x < pyramid.length - 1
+    new_alpha_val = val + pyramid[x + 1][y]
+    new_beta_val = val + pyramid[x + 1][y + 1]
+
+    recumagia(pyramid, x + 1, y, new_alpha_val)
+    recumagia(pyramid, x + 1, y + 1, new_beta_val)
+  else
+    @arroyo_seco << val
+  end
+end
+
+# recumagia(@pyramid018, 0, 0, @pyramid018[0][0])
+# puts @arroyo_seco.max
+
+
+
+
+def recuwrapmagia(pyramid, depth, x, y, val)
+  @arroyo_medio_seco = []
+
+  diepte = (depth < pyramid.length - 1 - x ? depth : pyramid.length - 1 - x)
+  puts "diepte = #{diepte}"
+
+  def recuexistencia(pyramid, depth, x, y, val)
+    if depth > 0
+      new_alpha_val = val + pyramid[x + 1][y]
+      new_beta_val = val + pyramid[x + 1][y + 1]
+
+      recuexistencia(pyramid, depth - 1, x + 1, y, new_alpha_val)
+      recuexistencia(pyramid, depth - 1, x + 1, y + 1, new_beta_val)
+    else
+      @arroyo_medio_seco << val
+    end
+  end
+
+  recuexistencia(pyramid, diepte, x, y, val)
+
+  return @arroyo_medio_seco.max
+
+end
+
+puts recuwrapmagia(@pyramid018, @pyramid018.length - 1, 0, 0, @pyramid018[0][0])
+puts "============"
+
+
+
+def calculamelecoso(pyramid, finesse)
+  pathofbuenagente = []
+  pathofbuenagente << pyramid[0][0]
+
+  row = 1
+  brick_position = 0
+
+  while row <= pyramid.length - 1
+    alpha = recuwrapmagia(pyramid, finesse, row, brick_position, pyramid[row][brick_position])
+    beta = recuwrapmagia(pyramid, finesse, row, brick_position + 1, pyramid[row][brick_position + 1])
+
+    if alpha > beta
+      pathofbuenagente << pyramid[row][brick_position]
+      puts pyramid[row][brick_position]
+    else
+      pathofbuenagente << pyramid[row][brick_position + 1]
+      puts pyramid[row][brick_position + 1]
+      brick_position += 1
+    end
+
+    row += 1
+  end
+
+  return pathofbuenagente.reduce(:+)
+end
+
+puts calculamelecoso(@pyramid018, 5)
+puts calculamelecoso(@pyramid067, 24)
+
+def find_biggest_path(pyramid)
+  pyr_size = pyramid.size
+  results = []
+  count = 0
+
+  while pyr_size > count
+    (0..count).each do |n|
+      puts pyramid[count][n]
+    end
+    count += 1
+  end
+end
+
+# find_biggest_path(pyramid018)
+
+
+
+
+
+
+
+
+
+
+
+=begin
+
+puts pyramid018[4][4]
+
+def findy(row, brick, pyramid)
+  [pyramid[row][brick] + pyramid[row + 1][brick], pyramid[row][brick] + pyramid[row + 1][brick + 1]]
+end
+
+pyrsize = pyramid018.size - 1
+
+pyrsize.each do |num|
+  findy(num, )
+end
+
+puts findy(0, 0, pyramid018).inspect
+puts findy(4, 3, pyramid018).inspect
+puts findy(8, 2, pyramid018).inspect
+=end
