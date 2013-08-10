cddded759609893c7877936e902cf16108edfd11
DrummerHead <drummerhead@gmail.com>
2013-08-09 21:27:43 -0300

    Add comments

--- 018.rb
+++ 018.rb
@@ -1,3 +1,7 @@
+# Description of problems:
+# http://projecteuler.net/problem=18
+# http://projecteuler.net/problem=67
+
 require './018_data.rb'
 require './067_data.rb'
 
@@ -53,3 +57,16 @@ end
 
 puts calculate_maximum_total(@pyramid018, 5)
 puts calculate_maximum_total(@pyramid067, 21)
+
+
+# This is a combination of polite brute force and thinking ahead. The best
+# analogy is the chess player who thinks several moves ahead to decide the next
+# move. In this case there is a scoped brute force with a "finesse" attribute
+# that takes a chunk of the pyramid, analyzes several moves ahead up to finesse
+# and then selects one of the two choices according to the maximum sum. The
+# winner goes through the same process until the pyramid is traversed
+# completely.
+#
+# For problem 67 the lowest number that yielded the correct value was 21.
+# Perhaps there's a more deterministic algorithm for this but I'm quite happy
+# with this result. Suggestions welcomed!
